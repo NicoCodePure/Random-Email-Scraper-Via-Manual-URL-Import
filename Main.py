a6ad47cb138e58dcb0409ba30b73529e020963e2
Nico Nicolaisen <110465986+NicoCodePure@users.noreply.github.com>
2023-04-11 23:41:44 +0200

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+import time
+import re
+
+PATH = 'C:\Program Files (x86)\chromedriver.exe'
+
+l=list()
+o={}
+
+target_url = 'https:example_email.com'
+
+driver=webdriver.Chrome(PATH)
+driver.get(target_url)
+
+email_pattern = r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,4}"
+html = driver.page_source
+emails=re.findall(email_pattern,html)
+
+time.sleep(5)
+print(emails)
+driver.close()
